dd86657ae603001b06b6c4f82d6122f554be69de
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-05-04 12:27:51 -0300

    Menu mobile construido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react"
+
 export default function Header() {
+
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+    function openMenu() {
+        if (menuIsOpen === false) {
+            setMenuIsOpen(true)
+        } else {
+            setMenuIsOpen(false)
+        }
+    }
 
     return (
         <>
@@ -35,7 +47,7 @@ export default function Header() {
                     <img className="h-full" src="../GGS_logo.png" alt="Logo" />
                 </a>
 
-                <div className="flex justify-around w-32">
+                <div className="flex justify-between items-center w-28">
                     <a href="">
                         <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -46,13 +58,34 @@ export default function Header() {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                         </svg>
                     </a>
-                    <svg className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
+
+                    {menuIsOpen ?
+                        <svg onClick={openMenu} className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                        :
+                        <svg onClick={openMenu} className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                    }
                 </div>
-
-
             </div>
+
+            {menuIsOpen ?
+                <>
+                    <hr className="h-px w-full" />
+                    <div className="w-screen h-screen bg-blue-500 flex justify-end">
+                        <ul className="flex flex-col mr-5 mt-3">
+                            <li className=" flex justify-end"><a className='menu-font' href="">Home</a></li>
+                            <li><a className='menu-font' href="">Consoles</a></li>
+                            <li className=" flex justify-end"><a className='menu-font' href="">Jogos</a></li>
+                            <li><a className='menu-font' href="">Gift card</a></li>
+                        </ul>
+                    </div>
+                </>
+                :
+                null
+            }
         </>
     )
 }
