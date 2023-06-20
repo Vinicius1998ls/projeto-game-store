d159749361c1a9d08a315ac1e64e74ef7cd3dbc6
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-06-20 09:18:58 -0300

    melhorias nos botões de navegação dos slider da pagina inicial

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,7 +11,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination , Navigation} from "swiper";
+import { Pagination, Navigation } from "swiper";
 
 const games = gamesList()
 const cards = cardList()
@@ -20,21 +20,21 @@ const consoles = consolesList()
 const list = games.concat(cards, consoles)
 
 function listNewProducts() {
-    
+
     let newItem = list.filter(item => {
         return item.new === true
     })
     return (newItem.map(item => {
         return (
             <SwiperSlide key={item.id}
-            style={{
-                marginLeft: '5px',
-                marginRight: '5px'
-            }}
+                style={{
+                    marginLeft: '5px',
+                    marginRight: '5px'
+                }}
             >
                 <div key={item.id} className="flex flex-col w-[130px] sm:w-36 h-fit mb-8 border-2 border-gray-200 rounded-md">
                     <Link href={`/products/${item.id}`}>
-                        <img className="object-contain w-full h-36 sm:h-44 lg:h-52 mt-1" src={item.img} alt="" />
+                        <img className="object-contain w-full h-36 sm:h-44 lg:h-52 mt-1 px-1" src={item.img} alt="" />
                         <h2 className="title-item text-base lg:text-lg text-center mx-1 sm:mx-5 mt-2 flex flex-wrap h-12 lg:h-14 overflow-hidden">
                             {item.name}{' '}
                             {item.platform ? item.platform : ''}
@@ -53,12 +53,12 @@ function listNewProducts() {
                 </div>
             </SwiperSlide>
         )
-    }))    
+    }))
 }
 
 function getMostWanted() {
     let mostWanted = list.filter(item => {
-        if(item.relevance >= 4) {
+        if (item.relevance >= 4) {
             return item
         }
     })
@@ -66,14 +66,14 @@ function getMostWanted() {
     return (mostWanted.map(item => {
         return (
             <SwiperSlide key={item.id}
-            style={{
-                marginLeft: '5px',
-                marginRight: '5px'
-            }}
+                style={{
+                    marginLeft: '5px',
+                    marginRight: '5px'
+                }}
             >
                 <div key={item.id} className="flex flex-col w-[130px] sm:w-36 h-fit mb-8 border-2 border-gray-200 rounded-md">
                     <Link href={`/products/${item.id}`}>
-                        <img className="object-contain w-full h-36 sm:h-44 lg:h-52 mt-1" src={item.img} alt="" />
+                        <img className="object-contain w-full h-36 sm:h-44 lg:h-52 mt-1 px-1" src={item.img} alt="" />
                         <h2 className="title-item text-base lg:text-lg text-center mx-1 sm:mx-5 mt-2 flex flex-wrap h-12 lg:h-14 overflow-hidden">
                             {item.name}{' '}
                             {item.platform ? item.platform : ''}
@@ -92,7 +92,7 @@ function getMostWanted() {
                 </div>
             </SwiperSlide>
         )
-    }))  
+    }))
 }
 
 export default function Home() {
@@ -104,65 +104,98 @@ export default function Home() {
                 <div className="flex justify-center ">
                     <h2 className="w-full min-[400px]:max-w-[400px] sm:max-w-[550px] lg:max-w-[850px] title-item text-xl mt-7 lg:mt-10 mx-5">Lançamentos:</h2>
                 </div>
-                <Swiper
-                    className="max-w-[300px] min-[400px]:max-w-[400px] sm:max-w-[600px] lg:max-w-[900px] mt-3 "
-                    style={{
-                        '--swiper-pagination-bottom': '5px;',
-                    }}
-                    slidesPerView={2}                                        
-                    navigation={true}
-                    breakpoints={{
-                        400: {
-                            slidesPerView: 3,                                                        
-                        },
-                        640: {
-                            slidesPerView: 3,
-                            slidesPerGroup: 2,                            
-                        },
-                        1024: {
-                            slidesPerView: 5,                            
-                        },
-                    }}
-                    modules={[Pagination, Navigation]}                    
-                >
-                    {listNewProducts()}
-                    <SwiperSlide>
-                        <div className="w-30"></div>
-                    </SwiperSlide>
-                </Swiper>
+
+                <div className="flex justify-center">
+                    <div className="absolute w-full min-[420px]:w-11/12 xl:w-10/12 2xl:w-8/12">
+                        <button id="new-products-prev"
+                            className={`swiper-button-prev p-7 rounded-full 
+                            translate-y-28 min-[400px]:translate-y-32 md:translate-y-40 lg:translate-y-48 
+                            min-[400px]:ml-[10%] sm:ml-[2%] min-[900px]:ml-[10%] lg:ml-0`}>
+                        </button>
+                        <button id="new-products-next"
+                             className={`swiper-button-next p-7 rounded-full
+                            translate-y-28 min-[400px]:translate-y-32 md:translate-y-40 lg:translate-y-48
+                            min-[400px]:mr-[10%] sm:mr-[2%] min-[900px]:mr-[10%] lg:mr-0 `}>
+                        </button>
+                    </div>
+                    <Swiper
+                        className="max-w-[300px] sm:max-w-[600px] lg:max-w-[900px] mt-3 "
+                        style={{
+                            '--swiper-pagination-bottom': '5px;',
+                        }}
+                        slidesPerView={2}
+                        navigation={{
+                            prevEl: '#new-products-prev',
+                            nextEl: '#new-products-next',
+                        }}
+                        breakpoints={{
+                            640: {
+                                slidesPerView: 3,
+                                slidesPerGroup: 2,
+                            },
+                            1024: {
+                                slidesPerView: 5,
+                            },
+                        }}
+                        modules={[Pagination, Navigation]}
+
+                    >
+                        {listNewProducts()}
+                        <SwiperSlide>
+                            <div className="w-30"></div>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+
+
                 <div className="flex justify-center ">
                     <h2 className="w-full min-[400px]:max-w-[400px] sm:max-w-[550px] lg:max-w-[850px] title-item text-xl mt-7 lg:mt-10 mx-5">Mais procurados:</h2>
                 </div>
-                <Swiper
-                    className="max-w-[300px] min-[400px]:max-w-[400px] sm:max-w-[600px] lg:max-w-[900px] mt-3 "
-                    style={{
-                        '--swiper-pagination-bottom': '5px;',
-                    }}
-                    slidesPerView={2}                                       
-                    navigation={true}
-                    breakpoints={{
-                        400: {
-                            slidesPerView: 3,                                                        
-                        },
-                        640: {
-                            slidesPerView: 3,
-                            slidesPerGroup: 2,
-                            
-                        },
-                        1024: {
-                            slidesPerView: 5,
-                            slidesPerGroup: 3,
-                            
-                        },
-                    }}
-                    modules={[Pagination, Navigation]}                    
-                >
-                    {getMostWanted()}
-                    <SwiperSlide>
-                        <div className="w-30"></div>
-                    </SwiperSlide>
-                </Swiper>
-                
+
+                <div className="flex justify-center">
+                    <div className="absolute w-full min-[420px]:w-11/12 xl:w-10/12 2xl:w-8/12">
+                        <button id="most-wanted-prev"
+                            className={`swiper-button-prev p-7 rounded-full 
+                            translate-y-28 min-[400px]:translate-y-32 md:translate-y-40 lg:translate-y-48 
+                            min-[400px]:ml-[10%] sm:ml-[2%] min-[900px]:ml-[10%] lg:ml-0`}>
+                        </button>
+                        <button id="most-wanted-next"
+                            className={`swiper-button-next p-7 rounded-full
+                            translate-y-28 min-[400px]:translate-y-32 md:translate-y-40 lg:translate-y-48
+                            min-[400px]:mr-[10%] sm:mr-[2%] min-[900px]:mr-[10%] lg:mr-0 `}>
+                        </button>
+                    </div>
+                    <Swiper
+                        className="max-w-[300px] sm:max-w-[600px] lg:max-w-[900px] mt-3 "
+                        style={{
+                            '--swiper-pagination-bottom': '5px;',
+                        }}
+                        slidesPerView={2}
+                        navigation={{
+                            prevEl: '#most-wanted-prev',
+                            nextEl: '#most-wanted-next',
+                        }}
+                        breakpoints={{
+                            640: {
+                                slidesPerView: 3,
+                                slidesPerGroup: 2,
+                            },
+                            1024: {
+                                slidesPerView: 5,
+                                slidesPerGroup: 4
+                            },
+                        }}
+                        modules={[Pagination, Navigation]}
+
+                    >
+                        {getMostWanted()}
+                        <SwiperSlide>
+                            <div className="w-30"></div>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+
+
             </main>
             <Footer></Footer>
         </>
